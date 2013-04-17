9d583b3ff569cd93c4daec7f1751c4a776cf59f6
Jeremy Yang <jeremyby@gmail.com>
2013-04-17 16:54:37 +0800

    switch to rubinius + puma

--- config/initializers/poll_categories.rb
+++
@@ -1,9 +0,0 @@
-Poll_Cats = {
-  "nil" => "pick one below",
-  "eco" => "Economy Perspective",
-  "cul" => "Culture, Art and Education",
-  "pol" => "Politics and Government",
-  "int" => "International",
-  "ppl" => "People Happiness"
-}
-
